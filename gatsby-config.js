cd5841f190725825d86cb4d03cca159fdb87becb
danielshow <danielshotonwa53@gmail.com>
2019-12-09 23:56:13 +0100

    using meta data with graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [`gatsby-plugin-emotion`]
+  siteMetadata: {
+    title: 'Gatsby Tutorial',
+    description: 'Learning Gatsby from front end masters'
+  },
+  plugins: [`gatsby-plugin-emotion`, 'gatsby-plugin-react-helmet']
 }
