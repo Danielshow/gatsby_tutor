41f6074ba868147a8ac1288bcc7e66468463f4c1
danielshow <danielshotonwa53@gmail.com>
2019-12-09 23:41:14 +0100

    adding styles and using styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Layout from '../components/layout';
+
 export default () => (
-  <>
+  <Layout>
+    <h1>Hello</h1>
     <div>Hello Frontend Masters!</div>
     <Link to='/about/'>Learn about me &rarr; </Link>
-  </>
+  </Layout>
 )
